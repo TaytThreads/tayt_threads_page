7e5fa7ea7b27ecd3c26779048a9416e09daa8816
murphy6867 <sonsawan.n1994@gmail.com>
2025-05-20 20:30:17 +0700

    fix(ci): fix ci and eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./routes/LandingPage";
-import AuthLayout from "./routes/Authentication";
-import SignIn from "./components/auth/SignIn";
-import Register from "./components/auth/Register";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const root = document.getElementById("root");
-if (!root) {
-  throw new Error("Failed to find the root element");
-}
-
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<LandingPage />} />
-
-      <Route path="/auth" element={<AuthLayout />}>
-        <Route path="login" element={<SignIn />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
