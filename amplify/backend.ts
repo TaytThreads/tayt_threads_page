146caf9a2620d7ce6320bad9ad58db1782fd9cbd
murphy6867 <sonsawan.n1994@gmail.com>
2025-05-28 11:37:46 +0700

    fix ap resource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,22 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
-  data,
 });
+
+const { cfnUserPool } = backend.auth.resources.cfnResources;
+
+cfnUserPool.policies = {
+  passwordPolicy: {
+    minimumLength: 6,
+    requireLowercase: false,
+    requireNumbers: false,
+    requireSymbols: false,
+    requireUppercase: false,
+    temporaryPasswordValidityDays: 1,
+  },
+};
